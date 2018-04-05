282fe71e393da9795967a631487c77fb338d5968
Elliot Strong <strong.elliot91@gmail.com>
2018-04-04 18:16:26 -0700

    final push of the day

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,17 +2,12 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def hello
-    render html: "Fuck off Bob
-    does the render come from GitHub
-    or local machine
-
-    Is this commited code going to make it to Heroku
-
-    or does it need to be pushed??
-
-    Test T or F
-
-
-    "
+    render html: "Fuck off Bob"
   end
+
+  def goodbye
+  render html:   "Oi mate./// SOD OFF"
+  end
+
+
 end
