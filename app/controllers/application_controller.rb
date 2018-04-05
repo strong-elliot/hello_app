4f99a1db4dd1fc0ef397412468b9aa89c006a152
Elliot Strong <strong.elliot91@gmail.com>
2018-04-04 18:00:56 -0700

    fu bob

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def hello
-    render html: "Hello World!"
+    render html: "Fuck off Bob"
   end
 end
